e5d377cc7838d96f4b0247f40d15da3e80c8e9f9
Jesus_Castillo <castillogonzalezj53@gmail.com>
2026-02-09 18:43:38 -0600

    test

--- idk.py
+++ idk.py
@@ -1,12 +1,24 @@
 import streamlit as st
+import random
 
 st.title("fnklkfnsakl")
 
 windows = ["1", "2", "3"]
 p1, p2, p3 = st.tabs(windows)
 
+num = random.randint(1, 100)
+
 with p1:
-    st.write("Texto de prueba")
+    while True:
+
+        att = st.number_input("Elige un numero: ")
+        if att < num:
+            st.write("Tu num es menor")
+        elif att > num:
+            st.write("Tu num es mayor")
+        else:
+            st.write("Has acertado")
+            break
 
 with p2:
     st.write("Texto de prueba lol")
