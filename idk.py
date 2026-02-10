a9c15499e7b8474e44bcf53a774ea4fabe693b47
Jesus_Castillo <castillogonzalezj53@gmail.com>
2026-02-09 18:38:12 -0600

    tests

--- idk.py
+++ idk.py
@@ -1,4 +1,15 @@
 import streamlit as st
 
 st.title("fnklkfnsakl")
-st.write("Texto de prueba")
+
+windows = ["1", "2", "3"]
+p1, p2, p3 = st.tabs(windows)
+
+with p1:
+    st.write("Texto de prueba")
+
+with p2:
+    st.write("Texto de prueba lol")
+
+with p3:
+    st.write("Texto de prueba faj")
