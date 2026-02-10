e0136fbe6ddad0cf0f9fe1a354fb3f952d2ef67b
Jesus_Castillo <castillogonzalezj53@gmail.com>
2026-02-09 18:50:21 -0600

    test

--- idk.py
+++ idk.py
@@ -3,6 +3,18 @@ import random
 
 st.title("fnklkfnsakl")
 
+def number_input(
+    label: str,
+    min_value: int | None = None,
+    max_value: int | None = None,
+    value: FloatOrNone@number_input | Literal['min'] = "min",
+    step: float | None = None,
+    format: str | None = None,
+    key: Key | None = None,
+    help: str | None = None,
+    on_change: WidgetCallback | None = None,
+    args: WidgetArgs | None = None,
+    kwargs: WidgetKwargs | None = None,
 windows = ["1", "2", "3"]
 p1, p2, p3 = st.tabs(windows)
 
